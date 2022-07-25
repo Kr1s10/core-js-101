6bef1ae4ac941c7b232f8a5e63ce028c81a7c380
Kristen <kr1s10.khud@gmail.com>
2022-07-26 02:14:30 +0300

    faet: add solution for evaluateTicTacToePosition

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -467,8 +467,18 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(p) {
+  for (let i = 0; i < p.length; i += 1) {
+    if (p[i][0] && p[i][0] === p[i][1] && p[i][0] === p[i][2]) return p[i][0];
+
+    if (p[0][i] && p[0][i] === p[1][i] && p[0][i] === p[2][i]) return p[0][i];
+  }
+
+  if (p[0][0] && p[0][0] === p[1][1] && p[0][0] === p[2][2]) return p[0][0];
+
+  if (p[0][2] && p[0][2] === p[1][1] && p[0][2] === p[2][0]) return p[0][2];
+
+  return undefined;
 }
 
 
